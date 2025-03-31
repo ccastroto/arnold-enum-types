934c01b52ee85e8b8bcc7026bac3ffd222dababc
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-03-31 14:39:26 +0200

    feat(arnoldmain): Comentar todo hasta que se necesite

--- Arnold/src/main/java/edu/asestatuas/main/ArnoldMain.java
+++ Arnold/src/main/java/edu/asestatuas/main/ArnoldMain.java
@@ -7,7 +7,7 @@ public class ArnoldMain {
     public static void main(String[] args) {
 
         double peso = 1.0; // kg
-
+        /*
         for(Planeta planeta : Planeta.values()){
             System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
         }
@@ -20,7 +20,7 @@ public class ArnoldMain {
         System.out.println("\nYour weight only on the gas giant planets: ");
         for(Planeta planeta: Planeta.getGigantesGaseosos()){
             System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-        }
+        }*/
     }
 
 }
