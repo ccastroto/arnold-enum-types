1efcc47f2ab9d4901d23bae3203b2d57a7165732
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-03-31 14:34:55 +0200

    feat(planeta): Definir lo que es masa y radio de los planetas

--- Arnold/src/main/java/edu/asestatuas/logica/Planeta.java
+++ Arnold/src/main/java/edu/asestatuas/logica/Planeta.java
@@ -13,6 +13,18 @@ public enum Planeta {
     URANUS(8.686e+25, 2.5559e7),
     NEPTUNE(1.024e+26, 2.4746e7);
 
+    private double masa = 0f;
+    private double radio = 0f;
 
+    private Planeta(double masa, double radio) {
+        this.masa = masa;
+        this.radio = radio;
+    }
+    public double getMasa() {
+        return this.masa;
+    }
+    public double getRadio() {
+        return this.radio;
+    }
 
 }
